a814d9aa01f2976099c51d655d3ff484ee85d8bc
jbenkual <joe@benkual.com>
2015-10-01 14:58:05 -0700

    Create template for viewing posts
    Define a post as either an image link or a text block
    Change display based on which type the post is

--- src/js/controllers/HomeCtrl.js
+++ src/js/controllers/HomeCtrl.js
@@ -1,5 +1,14 @@
 
 app.controller('HomeCtrl', function($scope) {
-  console.log("test")
-  $scope.test = "hello world";
+  var post = {
+    title: 'Hello world',
+    author: 'Cade',
+    subreddit: 'general',
+    createdAt: Date.now(),
+    score: 1,
+    content: 'https://myavantiservices.files.wordpress.com/2015/02/helloworld.gif',
+    comments: []
+  };
+
+  $scope.posts = [post, post, post];
 });
